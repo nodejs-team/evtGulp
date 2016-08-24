62edd547afc84cf5c6de48890416a578f2391a65
semdy <501859275@qq.com>
2016-08-24 18:23:19 +0800

    添加eccard banner右上角动画

--- src/eccard/js/eccard_banner.js
+++ src/eccard/js/eccard_banner.js
@@ -5,7 +5,7 @@
     var resData = {
         "groups":[
             {
-                "keys":"sq_png,q_png,q1_png,q2_png,q3_png,q4_png,q5_png,q6_png",
+                "keys":"sq_png,q_png,q1_png,q2_png,q3_png,q4_png,q5_png,q6_png,float_png",
                 "name":"preload"
             }],
         "resources":[
@@ -48,6 +48,11 @@
                 "name":"q6_png",
                 "type":"image",
                 "url":"banner/q6.png"
+            },
+            {
+                "name":"float_png",
+                "type":"image",
+                "url":"banner/float.png"
             }]
     };
 
@@ -167,6 +172,215 @@
             "944EDC10":{"x":154,"y":0,"w":148,"h":171},
             "6DBEFCB3":{"x":0,"y":173,"w":152,"h":171},
             "5DE45F63":{"x":304,"y":0,"w":148,"h":171}
+        }};
+
+    var floatMcData = {"mc":{
+        "float":{
+            "frameRate":24,
+            "events":[
+
+            ],
+            "frames":[
+
+                {
+                    "res":"19B6B2E7",
+                    "x":15,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"25550579",
+                    "x":0,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"F01A20BD",
+                    "x":0,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"A0C3EBDE",
+                    "x":0,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"21E0E272",
+                    "x":4,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"D352EA80",
+                    "x":13,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"6986894F",
+                    "x":15,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"4AB74D81",
+                    "x":15,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"AF466929",
+                    "x":15,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"4CB6870F",
+                    "x":15,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"A4E55CB",
+                    "x":12,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"7C101685",
+                    "x":8,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"C75260A0",
+                    "x":4,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"96A0FF64",
+                    "x":0,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"249C4E4B",
+                    "x":15,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"9D884DD6",
+                    "x":15,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"5B759411",
+                    "x":15,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"FFBCAEC6",
+                    "x":15,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"398DCE4E",
+                    "x":15,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"27FEAD56",
+                    "x":15,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"B39CDC04",
+                    "x":15,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"16457BF0",
+                    "x":15,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"C5435307",
+                    "x":15,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"CE4600D3",
+                    "x":15,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"5C59F80A",
+                    "x":15,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"E4B00FDE",
+                    "x":15,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"30B99635",
+                    "x":15,
+                    "y":7,
+                    "duration":1
+                },
+                {
+                    "res":"E9E757C",
+                    "x":15,
+                    "y":7,
+                    "duration":1
+                }
+            ]
+        }},
+        "res":{
+            "25550579":{"x":294,"y":0,"w":125,"h":111},
+            "7C101685":{"x":548,"y":0,"w":117,"h":111},
+            "F01A20BD":{"x":420,"y":113,"w":125,"h":111},
+            "AF466929":{"x":669,"y":339,"w":110,"h":111},
+            "6986894F":{"x":776,"y":226,"w":110,"h":111},
+            "27FEAD56":{"x":283,"y":339,"w":132,"h":111},
+            "CE4600D3":{"x":417,"y":339,"w":124,"h":111},
+            "A4E55CB":{"x":547,"y":226,"w":113,"h":111},
+            "30B99635":{"x":0,"y":0,"w":145,"h":111},
+            "4AB74D81":{"x":888,"y":226,"w":110,"h":111},
+            "5C59F80A":{"x":147,"y":339,"w":134,"h":111},
+            "B39CDC04":{"x":288,"y":226,"w":129,"h":111},
+            "4CB6870F":{"x":782,"y":113,"w":110,"h":111},
+            "9D884DD6":{"x":0,"y":113,"w":145,"h":111},
+            "FFBCAEC6":{"x":0,"y":226,"w":145,"h":111},
+            "21E0E272":{"x":669,"y":0,"w":121,"h":111},
+            "A0C3EBDE":{"x":293,"y":113,"w":125,"h":111},
+            "16457BF0":{"x":419,"y":226,"w":126,"h":111},
+            "5B759411":{"x":0,"y":339,"w":145,"h":111},
+            "E9E757C":{"x":147,"y":0,"w":145,"h":111},
+            "E4B00FDE":{"x":147,"y":113,"w":144,"h":111},
+            "C5435307":{"x":543,"y":339,"w":124,"h":111},
+            "249C4E4B":{"x":670,"y":113,"w":110,"h":111},
+            "398DCE4E":{"x":147,"y":226,"w":139,"h":111},
+            "C75260A0":{"x":547,"y":113,"w":121,"h":111},
+            "96A0FF64":{"x":421,"y":0,"w":125,"h":111},
+            "19B6B2E7":{"x":888,"y":0,"w":110,"h":111},
+            "D352EA80":{"x":662,"y":226,"w":112,"h":111}
         }};
 
     $.extend($.easing, {
@@ -384,6 +598,12 @@
         return mc;
     };
 
+    var setFloatMC = function(){
+        var mc = new MovieClip(Resource.getRes('float_png'), floatMcData, 'float', 'float');
+        mc.gotoAndPlay(1, -1);
+        return mc;
+    };
+
     var bindScroll = function( container ){
 
         var checkOffset = 200;
@@ -428,6 +648,7 @@
             Resource.el('#evt_loading').style.display = "none";
             Resource.el('#evt_container').style.display = 'block';
             correctPNG($('#evt_container').get(0));
+            setFloatMC();
             setSqMC();
             setQMC();
             bindScroll('#evt_container');
