8dfc913e3bfa46a38f75f33ca2d1074d4985dc75
王伟锋 <qpxt@163.com>
2017-01-18 18:28:49 +0800

    修改

--- src/loveday/js/loveday.js
+++ src/loveday/js/loveday.js
@@ -324,15 +324,15 @@
                 el.addEventListener('animationend', chainHandle, false);
             }
         } else {
-            if( aniMap[anim] ) {
-                aniMap[anim].call(el, el, delay, function(){
-                    if( chain ) {
-                        $(chain).each(function(){
-                            setAnimate(this, true);
-                        });
-                    }
-                });
-            }
+            // if( aniMap[anim] ) {
+            //     aniMap[anim].call(el, el, delay, function(){
+            //         if( chain ) {
+            //             $(chain).each(function(){
+            //                 setAnimate(this, true);
+            //             });
+            //         }
+            //     });
+            // }
         }
     };
     var bindScroll = function( container ){
@@ -381,7 +381,41 @@
                 return self.apply(context, [].concat.call(args, [].slice.call(arguments)));
             }
         };
-
+    var correctPNG = function(container){
+        var arVersion = navigator.appVersion.split("MSIE");
+        var version = parseFloat(arVersion[1]);
+        if (version && (version >= 5.5 && version < 9) && (document.body.filters)) {
+            var lee_i = 0;
+            var docimgs=container.getElementsByTagName('img');
+            for (var j = 0; j < docimgs.length; j++) {
+                var img = docimgs[j];
+                var imgName = img.src.toUpperCase();
+                if (imgName.substring(imgName.length - 3, imgName.length) == "PNG" && !img.getAttribute("usemap")) {
+                    lee_i++;
+                    var SpanID = img.id || 'ra_png_' + lee_i.toString();
+                    var imgData = new Image();
+                    imgData.proData = SpanID;
+                    imgData.onload = function () {
+                        // $("#" + this.proData).css("width", this.width + "px").css("height", this.height + "px");
+                    }
+                    imgData.src = img.src;
+                    var imgID = "id='" + SpanID + "' ";
+                    var imgClass = (img.className) ? "class='" + img.className + "' " : ""
+                    var imgTitle = (img.title) ? "title='" + img.title + "' " : "title='" + img.alt + "' "
+                    var imgStyle = "display:inline-block;" + img.style.cssText
+                    if (img.align == "left") imgStyle = "float:left;" + imgStyle
+                    if (img.align == "right") imgStyle = "float:right;" + imgStyle
+                    if (img.parentElement.href) imgStyle = "cursor:hand;" + imgStyle
+                    var strNewHTML = "<span " + imgID + imgClass + imgTitle
+                        + " style=\"" + "width:" + img.width + "px; height:" + img.height + "px;" + imgStyle + ";"
+                        + "filter:progid:DXImageTransform.Microsoft.AlphaImageLoader"
+                        + "(src=\'" + img.src + "\', sizingMethod='scale');\"></span>"
+                    img.outerHTML = strNewHTML;
+                    j = j - 1;
+                }
+            }
+        }
+    };
 
 
 
@@ -403,8 +437,8 @@
         ,moveClipEnd = false
     function initDomByScreenHeight(){
         headerHeight = $header.height();
-        winWidth = $win.width();
-        winHeight = $win.height();
+        winWidth = window.innerWidth || $win.width();
+        winHeight = window.innerHeight || $win.height();
         viewHeight = winHeight - headerHeight;
         containerWidth = winWidth>1920 ? 1920 : (winWidth>1280 ? winWidth : 1280);
         containerHeight = viewHeight>840 ? 840 : (viewHeight>500 ? viewHeight : 500);
@@ -441,9 +475,10 @@
     }
 
     function initRule(){
-        $('.rtitle').on('click', function(){
-            $('.rule').toggle();
-        })
+        $('.rule').show();
+        // $('.rtitle').on('click', function(){
+        //     $('.rule').toggle();
+        // })
     }
 
     function animateLoading(callback){
@@ -463,8 +498,13 @@
 
     function resourceComplete(){
         if(moveClipEnd && resLoaded){
+            if(!isSupportCss3){
+                $('[data-anim]').css('opacity',1)
+            }
             $('#blankdiv').show();
             $('#evt_content').show();
+            correctPNG($('.sec1').get(0));
+            correctPNG($('.sec2').get(0));
             $('.flower').parallax({
                 elementWidth : 1000,
                 elementHeight : 1000
