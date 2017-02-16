3a1d5177bbfacb29bdfeb94bc6381cbbbcb352af
shimilygood <1356300819@qq.com>
2017-02-16 09:25:20 +0800

    add women women-mym  demo newYear

---
+++ src/women/js/banner.js
@@ -0,0 +1,381 @@
+/**
+ * Created by mcake on 2016/5/24.
+ */
+(function($){
+    var resData = {
+        "groups":[
+            {
+                "keys":"arrow1_png,arrow2_png,bg_png,q1_png,q2_png,q3_png,q4_png,q5_png,text1_png,text2_png,title_png",
+                "name":"preload"
+            }],
+        "resources":[
+            {
+                "name":"arrow1_png",
+                "type":"image",
+                "url":"banner/arrow1.png"
+            },
+            {
+                "name":"arrow2_png",
+                "type":"image",
+                "url":"banner/arrow2.png"
+            },
+            {
+                "name":"bg_png",
+                "type":"image",
+                "url":"banner/bg.png"
+            },
+            {
+                "name":"q1_png",
+                "type":"image",
+                "url":"banner/q1.png"
+            },
+            {
+                "name":"q2_png",
+                "type":"image",
+                "url":"banner/q2.png"
+            },
+            {
+                "name":"q3_png",
+                "type":"image",
+                "url":"banner/q3.png"
+            },
+            {
+                "name":"q4_png",
+                "type":"image",
+                "url":"banner/q4.png"
+            },
+            {
+                "name":"q5_png",
+                "type":"image",
+                "url":"banner/q5.png"
+            },
+            {
+                "name":"text1_png",
+                "type":"image",
+                "url":"banner/text1.png"
+            },
+            {
+                "name":"text2_png",
+                "type":"image",
+                "url":"banner/text2.png"
+            },
+            {
+                "name":"title_png",
+                "type":"image",
+                "url":"banner/title.png"
+            }]
+    };
+
+    $.extend($.easing, {
+        easeInCubic:function(e,f,a,h,g){
+            return h*(f/=g)*f*f+a;
+        },
+        easeOutCubic: function(e,f,a,h,g){
+            return h*((f=f/g-1)*f*f+1)+a;
+        }
+    });
+
+    var aniMap = {
+        "slide-left": function(el, delay, cb){
+            var $el = $(el);
+            $el.css({
+                opacity: 0,
+                marginLeft: el.offsetWidth*0.5
+            });
+
+            setTimeout(function(){
+                $el.animate({
+                    opacity: 1,
+                    marginLeft: 0
+                }, 1000, 'easeOutCubic', function(){
+                    cb && cb();
+                });
+            }, delay);
+
+        },
+        "slide-right": function(el, delay, cb){
+            var $el = $(el);
+            $el.css({
+                opacity: 0,
+                marginLeft: -el.offsetWidth*0.5
+            });
+
+            setTimeout(function(){
+                $el.animate({
+                    opacity: 1,
+                    marginLeft: 0
+                }, 1000, 'easeOutCubic', function(){
+                    cb && cb();
+                });
+            }, delay);
+
+        },
+        "slide-down-l": function(el, delay, cb){
+            var $el = $(el);
+            $el.css({
+                opacity: 0,
+                marginLeft: -el.offsetWidth*0.3,
+                marginTop: -el.offsetHeight*0.8
+            });
+
+            setTimeout(function(){
+                $el.animate({
+                    opacity: 1,
+                    marginLeft: 0,
+                    marginTop: 0
+                }, 1000, 'easeOutCubic', function(){
+                    cb && cb();
+                });
+            }, delay);
+
+        },
+        "slide-down-r": function(el, delay, cb){
+            var $el = $(el);
+            $el.css({
+                opacity: 0,
+                marginLeft: el.offsetWidth*0.15,
+                marginTop: -el.offsetHeight*0.8
+            });
+
+            setTimeout(function(){
+                $el.animate({
+                    opacity: 1,
+                    marginLeft: 0,
+                    marginTop: 0
+                }, 1000, 'easeOutCubic', function(){
+                    cb && cb();
+                });
+            }, delay);
+
+        },
+        "slide-up-l": function(el, delay, cb){
+            var $el = $(el);
+            $el.css({
+                opacity: 0,
+                marginLeft: -el.offsetWidth*0.5,
+                marginTop: el.offsetHeight*0.5
+            });
+
+            setTimeout(function(){
+                $el.animate({
+                    opacity: 1,
+                    marginLeft: 0,
+                    marginTop: 0
+                }, 1000, 'easeOutCubic', function(){
+                    cb && cb();
+                });
+            }, delay);
+
+        },
+        "slide-up-r": function(el, delay, cb){
+            var $el = $(el);
+            $el.css({
+                opacity: 0,
+                marginLeft: el.offsetWidth*0.5,
+                marginTop: el.offsetHeight*0.5
+            });
+
+            setTimeout(function(){
+                $el.animate({
+                    opacity: 1,
+                    marginLeft: 0,
+                    marginTop: 0
+                }, 1000, 'easeOutCubic', function(){
+                    cb && cb();
+                });
+            }, delay);
+
+        },
+        "slide-up": function(el, delay, cb){
+            var $el = $(el);
+            $el.css({
+                opacity: 0,
+                //marginRight: -el.offsetWidth*0.5,
+                marginTop: el.offsetHeight*0.5
+            });
+
+            setTimeout(function(){
+                $el.animate({
+                    opacity: 1,
+                    // marginRight: 0,
+                    marginTop: 0
+                }, 1000, 'easeOutCubic', cb);
+            }, delay);
+
+        },
+        "fade-in": function(el, delay, cb){
+            var $el = $(el);
+            $el.css({
+                opacity: 0
+            });
+
+            setTimeout(function(){
+                $el.animate({
+                    opacity: 1
+                }, 800, cb);
+            }, delay);
+        }
+    };
+
+    var isSupportCss3 = (function(){
+        var ret = /MSIE (\d+\.\d+)/.exec(navigator.userAgent);
+        if( !ret || ret[1] > 9 ){
+            return true;
+        }
+        return false;
+    })();
+
+    var setAnimate = function(el, hasDelay){
+        var anim = el.getAttribute('data-anim');
+        var delay = Number(el.getAttribute('data-delay')||0)*1000;
+        var delayAdjust = Number(el.getAttribute('data-delay-adjust')||0)*1000;
+        var chain = el.getAttribute('data-chain');
+
+        delay = hasDelay ? delay : 0;
+        delay += delayAdjust;
+
+        if( isSupportCss3 ){
+            var chainHandle = function(){
+                $(chain).each(function(){
+                    setAnimate(this, true);
+                });
+                el.removeEventListener('webkitAnimationEnd', chainHandle, false);
+                el.removeEventListener('animationend', chainHandle, false);
+            };
+
+            el.className = [el.className, anim].join(" ");
+            el.style['-webkit-animation-delay'] = delay + "ms";
+            el.style['animationDelay'] = delay + "ms";
+            if( chain ) {
+                el.addEventListener('webkitAnimationEnd', chainHandle, false);
+                el.addEventListener('animationend', chainHandle, false);
+            }
+        } else {
+            if( aniMap[anim] ) {
+                aniMap[anim].call(el, el, delay, function(){
+                    if( chain ) {
+                        $(chain).each(function(){
+                            setAnimate(this, true);
+                        });
+                    }
+                });
+            }
+        }
+    };
+
+    var setAllAnimate = function(container){
+        $(container).find("[data-anim]").each(function(){
+            setAnimate(this);
+        });
+    };
+
+    var bindScroll = function( container ){
+
+        var checkOffset = 0;
+        var $win = $(window);
+        var winHeight = $win.height();
+        var elems = $(container).find('[data-anim]');
+        var elemObj = [];
+
+        elems.each(function(){
+            elemObj.push({
+                $elem: $(this),
+                anim: this.getAttribute('data-anim'),
+                scrollTop: $(this).offset().top,
+                checkOffset: Number(this.getAttribute('data-offset')),
+                isAnimated: false
+            });
+        });
+
+        $win.on('scroll', function(){
+            var scrollTop = $win.scrollTop();
+            var docHeight = document.documentElement.clientHeight;
+
+            $.each(elemObj, function(i, obj){
+                if( !obj.isAnimated ) {
+                    if( obj.$elem[0].getAttribute('data-ignore') ){
+                        obj.isAnimated = true;
+                        return;
+                    }
+                    if (scrollTop + docHeight - checkOffset - obj.checkOffset > obj.scrollTop) {
+                        setAnimate(obj.$elem[0], obj.scrollTop < winHeight);
+                        obj.isAnimated = true;
+                    }
+                }
+            });
+        })
+            .trigger('scroll');
+    };
+
+    Function.prototype.bind = Function.prototype.bind || function(){
+            var self = this,
+                context = [].shift.call(arguments),
+                args = [].slice.call(arguments);
+            return function(){
+                return self.apply(context, [].concat.call(args, [].slice.call(arguments)));
+            }
+        };
+
+    var loadResource = function(){
+
+        var loadComplete = function () {
+            Resource.el('#evt_loading').style.display = "none";
+            Resource.el('#evt_container').style.display = 'block';
+            correctPNG($('#evt_container').get(0));
+            bindScroll('#evt_container');
+        };
+        var loader = new Resource.loadGroup("preload", resData);
+        var spin = Resource.el('#evt_spin');
+
+        loader.on("progress", function (loaded, total) {
+            spin.innerHTML = "loading: " + Math.floor(loaded / total * 100) + "%";
+        });
+
+        loader.on("complete", loadComplete);
+    };
+
+    var correctPNG = function(container){
+        var arVersion = navigator.appVersion.split("MSIE");
+        var version = parseFloat(arVersion[1]);
+        if (version && (version >= 5.5 && version < 9) && (document.body.filters)) {
+            var lee_i = 0;
+            var docimgs=container.getElementsByTagName('img');
+            for (var j = 0; j < docimgs.length; j++) {
+                var img = docimgs[j];
+                var imgName = img.src.toUpperCase();
+                if (imgName.substring(imgName.length - 3, imgName.length) == "PNG" && !img.getAttribute("usemap")) {
+                    lee_i++;
+                    var SpanID = img.id || 'ra_png_' + lee_i.toString();
+                    var imgData = new Image();
+                    imgData.proData = SpanID;
+                    imgData.onload = function () {
+                        $("#" + this.proData).css("width", this.width + "px").css("height", this.height + "px");
+                    }
+                    imgData.src = img.src;
+                    var imgID = "id='" + SpanID + "' ";
+                    var imgClass = (img.className) ? "class='" + img.className + "' " : ""
+                    var imgTitle = (img.title) ? "title='" + img.title + "' " : "title='" + img.alt + "' "
+                    var imgStyle = "display:inline-block;" + img.style.cssText
+                    if (img.align == "left") imgStyle = "float:left;" + imgStyle
+                    if (img.align == "right") imgStyle = "float:right;" + imgStyle
+                    if (img.parentElement.href) imgStyle = "cursor:hand;" + imgStyle
+                    var strNewHTML = "<span " + imgID + imgClass + imgTitle
+                        + " style=\"" + "width:" + img.width + "px; height:" + img.height + "px;" + imgStyle + ";"
+                        + "filter:progid:DXImageTransform.Microsoft.AlphaImageLoader"
+                        + "(src=\'" + img.src + "\', sizingMethod='scale');\"></span>"
+                    img.outerHTML = strNewHTML;
+                    j = j - 1;
+                }
+            }
+        }
+    };
+
+    $(function(){
+        loadResource();
+    });
+
+    if( navigator.userAgent.indexOf("Edge") != -1 ){
+        document.body.className += "browser-edge";
+    }
+
+})(jQuery);
