96b874dc1040bd227274c9a58d4568e3975a49b1
semdy <501859275@qq.com>
2017-09-29 11:20:36 +0800

    打包工具优化

--- gulpfile.js
+++ gulpfile.js
@@ -167,8 +167,8 @@ var buildHTML = lazypipe()
         },
         cssSelector: 'style[data-minify!="false"]'
     })
-    .pipe(replace, /"(css|images|js)\/([^"]+?)"/gm, '"' + config.revPrefix + config.projectName + "/" + '$1/$2' + '"')
-    .pipe(replace, /(:\s*url\()(\.\.)?([^)]+?)/gm, '$1' + config.revPrefix + config.projectName + '/$3');
+    .pipe(replace, /(:\s*url\(['"]?)(\.\.)?([^)]+?)/gm, '$1' + config.revPrefix + config.projectName + '/$3')
+    .pipe(replace, /(['"])(css|images|js)\/([^"]+?)(['"])/gm, '$1' + config.revPrefix + config.projectName + "/" + '$2/$3' + '$4');
 
 var buildJS = lazypipe()
     .pipe(uglify)
