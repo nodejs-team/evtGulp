732d175b3e9c00a0972bb9ceff44f5793d439a05
shimilygood <1356300819@qq.com>
2018-03-30 14:38:30 +0800

    2018-3-30 backup

---
+++ src/2018/shengbang-caomei/js/main.js
@@ -0,0 +1,54 @@
+/**
+ * Created by mcake on 2016/5/24.
+ */
+(function($){
+
+
+
+
+
+  var loadComplete = function () {
+    $(".select li").click(function () {
+      $(this).addClass('on').siblings().removeClass('on');
+    });
+
+    
+  };
+
+  var loadResource = function(){
+    if( typeof resData == 'object' && Array.isArray(resData.resources) && resData.resources.length > 0 ){
+      startLoader(resData);
+    } else {
+      var resLoader = new Resource.JSONloader('res.json');
+      resLoader.on("success", function (res) {
+        startLoader(res);
+      });
+      resLoader.on("error", function () {
+        console.error("资源配置加载失败...");
+      });
+    }
+
+    function startLoader(data) {
+      var loader = new Resource.loadGroup("preload", data);
+      var spin = Resource.el('#evt_spin');
+
+      loader.on("progress", function (loaded, total) {
+        spin.innerHTML = "loading: " + Math.floor(loaded / total * 100) + "%";
+      });
+
+      loader.on("complete", function () {
+        Resource.el('#evt_loading').style.display = "none";
+        Resource.el('#evt_container').style.display = 'block';
+        correctPNG($('#evt_container').get(0));
+        bindScroll('#evt_container');
+        loadComplete();
+      });
+    }
+
+  };
+
+  $(function(){
+    loadResource();
+  });
+
+})(jQuery);
