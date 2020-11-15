46b3344ce6540d7a9b80c359c9265f277085c4ce
fangli <1356300819@qq.com>
2020-11-16 07:52:53 +0800

    11-13上传

--- src/2020/christmas/js/main.js
+++ src/2020/christmas/js/main.js
@@ -3,55 +3,10 @@
  */
 (function($){
 
-  var animations= {
-    window: function () {
-      var mc = new MovieClip('window_png', "window_json", 'el_window');
-      mc.gotoAndPlay(1, -1);
-      /*"duration":6 = 0.3*20*/
-      return mc;
-    }
-  };
-
-  var audioPlay = function () {
-    var audio = document.getElementById('audio');
-      audio.play();
-      var on = true;
-
-      if(!audio.paused){
-        on=false;
-        $(".music-btn").removeClass("on");
-      }else{
-        on=true;
-        $(".music-btn").addClass("on");
-      }
-
-      $(".music-btn").click(function () {
-        if(on){
-          on=false;
-          audio.play();
-          $(this).removeClass("on");
-        }else {
-          on=true;
-          audio.pause();
-          $(this).addClass("on");
-        }
-      });
-
-  }
-
-  window.audioPlay = audioPlay;
 
   var loadComplete = function () {
 
-    animations.window();
-    var swiper2 = new Swiper('.swiper2', {
-      pagination: '.pagination2',
-      grabCursor: true,
-      loop:true,
-      autoplay : 3000,
-      autoplayDisableOnInteraction : false,
-      paginationClickable: true
-    });
+
 
 
   };
